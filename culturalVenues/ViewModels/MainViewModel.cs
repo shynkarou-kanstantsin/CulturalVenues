3b16214818bdea5fc4b7ceffd7df378ec973b946
shynkarou-kanstantsin <shynkarou.kanstantsin@gmail.com>
2026-01-14 17:59:15 +0300

    Added trial recommendations carousel.

--- culturalVenues/ViewModels/MainViewModel.cs
+++ culturalVenues/ViewModels/MainViewModel.cs
@@ -8,7 +8,7 @@ namespace CulturalVenues.ViewModels
     internal class MainViewModel : ObservableObject
     {
         public ObservableCollection<Event> Events { get; set; }
-        
+
 
         public MainViewModel()
         {
@@ -55,10 +55,73 @@ namespace CulturalVenues.ViewModels
                         Address = "456 Music Ave, Los Angeles, CA",
                         PhotoUrl = new List<string> { "https://example.com/venue2.jpg" }
                     }
+                },
+                new Event
+                {
+                    Id = "3",
+                    Title = "Theater Play",
+                    Description = "A captivating drama performed by local actors.",
+                    Date = new DateTime(2024, 9, 10),
+                    TimeStart = new TimeSpan(19, 30, 0),
+                    StartingPrice = 15.00m,
+                    Currency = "USD",
+                    PhotoUrl = new List<string> { "https://www.maly.ru/images/performances/5a43aa03ea87e.jpg" },
+                    Type = "Theater",
+                    Venue = new Venue
+                    {
+                        Id = "V3",
+                        Name = "Downtown Theater",
+                        Latitude = 41.8781,
+                        Longitude = -87.6298,
+                        Address = "789 Drama Rd, Chicago, IL",
+                        PhotoUrl = new List<string> { "https://example.com/venue3.jpg" }
+                    }
+                },
+                new Event
+                {
+                    Id = "4",
+                    Title = "Jazz Night",
+                    Description = "A night of smooth jazz performances.",
+                    Date = new DateTime(2024, 10, 5),
+                    TimeStart = new TimeSpan(21, 0, 0),
+                    StartingPrice = 20.00m,
+                    Currency = "USD",
+                    PhotoUrl = new List<string> { "https://showcatalog.by/Shutterstock/Jazz-Banner.jpg" },
+                    Type = "Concert",
+                    Venue = new Venue
+                    {
+                        Id = "V4",
+                        Name = "Jazz Club",
+                        Latitude = 29.7604,
+                        Longitude = -95.3698,
+                        Address = "321 Jazz Ln, Houston, TX",
+                        PhotoUrl = new List<string> { "https://example.com/venue4.jpg" }
+                    }
+                },
+                new Event
+                {
+                    Id = "5",
+                    Title = "Modern Dance Performance",
+                    Description = "A contemporary dance show by a renowned troupe.",
+                    Date = new DateTime(2024, 11, 12),
+                    TimeStart = new TimeSpan(19, 0, 0),
+                    StartingPrice = 30.00m,
+                    Currency = "USD",
+                    PhotoUrl = new List<string> { "https://godance.tv/sites/default/files/users/1/liteblog/122/084D8CD4-7492-405E-B31D-462E4ADC585A.jpeg" },
+                    Type = "Dance",
+                    Venue = new Venue
+                    {
+                        Id = "V5",
+                        Name = "City Dance Theater",
+                        Latitude = 33.4484,
+                        Longitude = -112.0740,
+                        Address = "654 Dance Blvd, Phoenix, AZ",
+                        PhotoUrl = new List<string> { "https://example.com/venue5.jpg" }
+                    }
                 }
             };
         }
-
+            
 
     }
 }
