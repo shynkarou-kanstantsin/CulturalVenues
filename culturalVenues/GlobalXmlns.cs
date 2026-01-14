a30bc50581d2eab6ef6a8907df4384937aa70869
shynkarou-kanstantsin <shynkarou.kanstantsin@gmail.com>
2026-01-14 13:06:40 +0300

    Добавьте файлы проекта.

---
+++ culturalVenues/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CulturalVenues")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CulturalVenues.Pages")]
