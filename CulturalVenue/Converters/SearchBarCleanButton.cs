842d87c8f34d1f258be937cafea52b0b5fb1a814
shynkarou-kanstantsin <shynkarou.kanstantsin@gmail.com>
2026-01-19 14:22:19 +0300

    Updated X button logic of a search bar.

---
+++ CulturalVenue/Converters/SearchBarCleanButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace CulturalVenue.Converters
+{
+    internal class SearchBarCleanButton : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is string text && !string.IsNullOrWhiteSpace(text);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CulturalVenue/Views/Controls/SearchBar.xaml.cs
+++ CulturalVenue/Views/Controls/SearchBar.xaml.cs
@@ -1,9 +1,34 @@
+using System.ComponentModel;
+using System.Windows.Input;
+
 namespace CulturalVenue.Views.Controls;
 
 public partial class SearchBar : ContentView
 {
-	public SearchBar()
+	public static readonly BindableProperty TextProperty = 
+		BindableProperty.Create(
+		nameof(Text),
+		typeof(string),
+		typeof(SearchBar),
+		string.Empty,
+		BindingMode.TwoWay);
+
+	public string Text
+	{
+		get => (string)GetValue(TextProperty);
+		set => SetValue(TextProperty, value);
+    }
+
+    public SearchBar()
 	{
 		InitializeComponent();
-	}
+		ClearCommand = new Command(ClearText);
+    }
+
+	public ICommand ClearCommand { get; }
+
+	public void ClearText()
+	{
+		Text = string.Empty;
+    }
 }
